7ee8a46b9400b546b882a13361486b784005f5ba
JuanfraDelRM <139347667+juanfradelrm@users.noreply.github.com>
2025-06-02 13:11:45 +0200

    Update P5_script.sh

--- P5_script.sh
+++ P5_script.sh
@@ -1,14 +1,18 @@
-!/bin/bash
+#!/bin/bash
 
-echo "======================================================="
-echo "Validación Práctica 5 - Infraestructura de red virtual"
-echo "======================================================="
-echo " "
+echo "=================================================================================================================================================="
+echo "Validación Práctica 5 - Virtualización y Almacenamiento"
+echo "=================================================================================================================================================="
 
-# Tarea 1: Creación de red NAT CLuster
+# Variables
 mv=mvp5
 RED_1="Cluster"
 RED_2="Almacenamiento"
+RED_LIBVIRT="Cluster"
+IP_INICIO_ESPERADA="192.168.140.2"
+IP_FINAL_ESPERADA="192.168.140.149"
+
+# Tarea 1: Creación de red NAT CLuster
 
 echo "Tarea 1: Creacion de la red NAT $RED_1"
 echo "Comprobando que existe la red NAT $RED_1..."
@@ -25,9 +29,6 @@ fi
 echo " "
 echo "Comprobando si el rango de direcciones se encuentra entre 192.168.140.2 – 192.168.140.149..."
 
-RED_LIBVIRT="Cluster"
-IP_INICIO_ESPERADA="192.168.140.2"
-IP_FINAL_ESPERADA="192.168.140.149"
 
 DHCP_BLOQUE=$(virsh net-dumpxml "$RED_LIBVIRT" | awk '/<dhcp>/,/<\/dhcp>/')
 IP_INICIO_REAL=$(echo "$DHCP_BLOQUE" | grep -oP "<range start='\K[^']+")
